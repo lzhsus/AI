1922e2c0685cb6bb95521a065358829e4808ce86
lzhsus <1434253600@qq.com>
2021-08-03 16:11:20 +0800

    '0803'

--- miniprogram/caipiao/history/history.js
+++ miniprogram/caipiao/history/history.js
@@ -42,18 +42,52 @@ Page({
                 }
                 arr.forEach(item=>{
                     if(item.win_code&&item.win_code.length){
-                        let list01 = item.win_code.slice(0,5)
-                        let list02 = item.win_code.slice(5)
-                        item.list2.forEach(obj=>{
+                        let list01 = item.win_code.slice(0,5);
+                        let list02 = item.win_code.slice(5);
+                        item.list2 = item.list2.map(obj=>{
+                            let oneNum = 0;
+                            let towNum = 0;
                             obj.list.forEach(element=>{
                                 element.show = false;
                                 if(element.type==1){
-                                    if(list01.indexOf(element.num)!=-1) element.show = true;
+                                    if(list01.indexOf(element.num)!=-1){
+                                        element.show = true
+                                        oneNum++
+                                    };
                                 }
                                 if(element.type==2){
-                                    if(list02.indexOf(element.num)!=-1) element.show = true;
+                                    if(list02.indexOf(element.num)!=-1){
+                                        element.show = true
+                                        towNum++
+                                    };
                                 }
                             })
+                            obj.price = 0
+                            if(oneNum==5&&oneNum==2){
+                                obj.price = '1A+0'
+                            }else if(oneNum==5&&oneNum==1){
+                                obj.price = '1B+0'
+                            }else if(oneNum==5&&oneNum==0){
+                                obj.price = 10000
+                            }else if(oneNum==4&&oneNum==2){
+                                obj.price = 3000
+                            }else if(oneNum==4&&oneNum==1){
+                                obj.price = 300
+                            }else if(oneNum==4&&oneNum==0){
+                                obj.price = 100
+                            }else if(oneNum==3&&oneNum==2){
+                                obj.price = 200
+                            }else if(oneNum==3&&oneNum==1||oneNum==2&&oneNum==2){
+                                obj.price = 15
+                            }else if(oneNum==3&&oneNum==0||oneNum==2&&oneNum==1||oneNum==1&&oneNum==2||oneNum==0&&oneNum==2){
+                                obj.price = 5
+                            }else{
+                                obj.price = 0
+                            }
+                            obj.price = obj.price+''
+                            console.log(obj.price)
+                            console.log(oneNum,towNum)
+                            return obj;
                         })
                     }
                 })
@@ -85,6 +119,16 @@ Page({
         })
         this.setData({
             day2:item.day2
+        })
+    },
+    openChangeRule(){
+        this.setData({
+            popShow:'rule'
+        })
+    },
+    cloreChangeRule(){
+        this.setData({
+            popShow:''
         })
     },
     inputCode(e){
